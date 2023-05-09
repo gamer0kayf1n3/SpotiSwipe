3d8a08b083bcf9f827cbb396c489412ecc741c95
gamer0kayf1n3 <81130943+gamer0kayf1n3@users.noreply.github.com>
2023-05-09 20:20:23 +0800

    up +down

--- main.py
+++ main.py
@@ -16,11 +16,22 @@ def left():
 def right():
     pyautogui.press("nexttrack")
     return ''
+@app.route('/up')
+def up():
+    pyautogui.press("volumeup")
+    pyautogui.press("volumeup")
+    return ''
 
+@app.route('/down')
+def down():
+    pyautogui.press("volumedown")
+    pyautogui.press("volumedown")
+    return ''
 @app.route('/tap')
 def tap():
     pyautogui.press('playpause')
     return ''
-
+pyautogui. FAILSAFE = False
+pyautogui.PAUSE = 0
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0")
