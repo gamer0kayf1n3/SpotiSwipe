5e1c044c8bf92dec596c0cfd8195c3396ca0e0d4
BermudaVI <bermudezkylenathan@gmail.com>
2023-12-15 13:19:29 +0800

    fader addition

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pyautogui
-
+from pycaw.pycaw import AudioUtilities, ISimpleAudioVolume
+sessions = AudioUtilities.GetAllSessions()
+def vol(i):
+    for session in sessions:
+        volume = session._ctl.QueryInterface(ISimpleAudioVolume)
+        if session.Process:
+            print("volume.GetMasterVolume(): %s" % volume.GetMasterVolume())
+            volume.SetMasterVolume(int(i)/100, None)
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('main.html')
+@app.route('/slider')
+def skdier():
+    return render_template('volume.html')
+@app.route('/volume', methods=["POST"])
+def vols():
+    data = request.get_json()
+    volume = data.get('volume')
+    # Here, you can handle the volume data as needed (e.g., store in a database, perform actions, etc.)
+    vol(volume)
+    return jsonify({'message': 'Volume received successfully'})
 
 @app.route('/left')
 def left():
